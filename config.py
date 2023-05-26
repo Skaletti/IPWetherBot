27af7cf93b94ac40a6c2936aacc96de5144b09e1
Skaletti <98595208+Skaletti@users.noreply.github.com>
2023-05-26 08:28:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_TOKEN = '6253511492:AAHKve6QP85DPE3dYw-Z2TvJpWQu7i2Nuww'
+API_TOKEN = ''
 
-WEATHER_API_KEY = '28206f1ba9de00604b7f7956cd663c54'
+WEATHER_API_KEY = ''
 
 CURRENT_WEATHER_API_CALL = (
         'https://api.openweathermap.org/data/2.5/weather?'
